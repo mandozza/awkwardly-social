6b166357df967d0e96bcacca7d29a89d84ce4047
Jeremy Josey <jjosey@getchalk.com>
2019-12-13 13:29:19 -0400

    Added the Ability to post to the blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar'
+import Main from './Main'
 import Web3 from 'web3';
 import SocialNetwork from '../abis/SocialNetwork.json'
 import './App.css';
@@ -30,17 +31,17 @@ class App extends Component {
     const web3 = window.web3
     //Load Account
     const accounts = await web3.eth.getAccounts()
-    console.log(accounts)
+    console.log(accounts,'ACCOUNT')
     this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
     console.log(networkId, 'networkid')
     const networkData = SocialNetwork.networks[networkId]
-    console.log(networkData)
+    console.log(networkData, 'Network Data')
     if(networkData) {
       const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
       this.setState({ socialNetwork })
       const postCount = await socialNetwork.methods.postCount().call() 
-      console.log(postCount)
+      console.log(postCount, "postCount")
       this.setState({ postCount })
 
       //load Posts.
@@ -50,11 +51,26 @@ class App extends Component {
           posts: [...this.state.posts, post]
         })
       }
+      this.setState({ loading: false})
       console.log({ posts: this.state.posts })
     } else {
       window.alert('SocialNetwork contract has not been deployed to the network.')
     }
   }
+
+
+  createPost(content) {
+    console.log('create post')
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .then(function(receipt){
+      console.log('receipt recieved')
+      this.setState({ loading: false })
+    })
+  }
+
+
+
 
   constructor(props) {
     super(props)
@@ -62,44 +78,23 @@ class App extends Component {
       account: '',
       socialNetwork: null,
       postCount: 0,
-      posts: []
+      posts: [],
+      loading: true
     }
+    this.createPost = this.createPost.bind(this)
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account}/>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
-              <div className="content mr-auto ml-auto">
-                  { this.state.posts.map((post,key) => {
-                      return (
-                        <div className="card mb-4" key={key} >
-                            <div className="card-header">
-                              <small className="text-muted">{post.author}</small>
-                            </div>
-                            <ul id="postList" className="list-group list-group-flush">
-                                <li className="list-group-item">
-                                  <p>{post.content}</p>
-                                </li>
-                                <li key={key} className="list-group-item py-2">
-                                    <small className="float-left mt-1 text-muted">
-                                      Tips: { window.web3.utils.fromWei(post.tipAmount.toString(), 'ether')} ETH
-                                    </small>
-                                    <button className="btn btn-link btn-sm float-right pt-0">
-                                      <span>TIP 0.1 ETH</span>
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                      )
-                  })}
-              </div>
-            </main>
-          </div>
-        </div>
+        { this.state.loading 
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main 
+               posts={this.state.posts }
+               createPost = {this.createPost}
+          />
+        }
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import React,  { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+              <div className="content mr-auto ml-auto">
+              <p>&nbsp;</p>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const content = this.postContent.value
+                  this.props.createPost(content)
+                }}>
+              <div className="form-group mr-sm-2">
+                  
+                  <input
+                    id="postContent"
+                    type="text"
+                    ref={(input) => { this.postContent = input }}
+                    className="form-control"
+                    placeholder="What's on your mind?"
+                    required />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Share</button>
+              </form>
+              <p>&nbsp;</p>
+                  { this.props.posts.map((post,key) => {
+                      return (
+                        <div className="card mb-4" key={key} >
+                            <div className="card-header">
+                              <small className="text-muted">{post.author}</small>
+                            </div>
+                            <ul id="postList" className="list-group list-group-flush">
+                                <li className="list-group-item">
+                                  <p>{post.content}</p>
+                                </li>
+                                <li key={key} className="list-group-item py-2">
+                                    <small className="float-left mt-1 text-muted">
+                                      Tips: { window.web3.utils.fromWei(post.tipAmount.toString(), 'ether')} ETH
+                                    </small>
+                                    <button className="btn btn-link btn-sm float-right pt-0">
+                                      <span>TIP 0.1 ETH</span>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                      )
+                  })}
+              </div>
+            </main>
+          </div>
+        </div>
+
+        );
+    }
+}
+
+export default Main;
